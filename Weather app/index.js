ceaee08a79bc1e0d1b0fc12eeffb41729216cbc8
CaioVitorDM <112642203+CaioVitorDM@users.noreply.github.com>
2023-03-21 15:16:10 -0300

    Update index.js

--- Weather app/index.js
+++ Weather app/index.js
@@ -47,7 +47,7 @@ search.addEventListener('click', ()=>{
                     image.src = 'images/cloud.png';
                     break;
 
-                case 'Haze':
+                case 'Mist':
                     image.src = 'images/mist.png';
                     break;
                     
